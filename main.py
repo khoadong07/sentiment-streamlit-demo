2d0c8580c825673b721a01561b3c62e4808b29b1
khoadong07 <khoadong07@gmail.com>
2024-12-25 09:10:41 +0000

    update new model

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, Auto
 import time
 from io import BytesIO
 
+st.set_option('server.port', 8501)
+st.set_option('server.enableCORS', False)
+
 # Function to export DataFrame to Excel
 def export_to_excel(dataframe):
     output = BytesIO()
